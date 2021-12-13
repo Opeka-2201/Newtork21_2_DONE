51ae6bfea4a3c611005769afb95b67759aac0c80
Opeka-2201 <65912187+Opeka-2201@users.noreply.github.com>
2021-12-13 16:08:27 +0100

    16h

--- ReaderBroker.java
+++ ReaderBroker.java
@@ -1,4 +1,7 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.concurrent.BlockingQueue;
 
 /**
@@ -8,15 +11,35 @@ public class ReaderBroker implements Runnable {
 
     Socket s;
     BlockingQueue<byte[]> queue;
+    InputStream in;
     
-    public ReaderBroker(Socket s, BlockingQueue<byte[]> q){
+    public ReaderBroker(Socket s, BlockingQueue<byte[]> q) throws IOException{
         this.queue = q;
         this.s = s;
+        this.in = s.getInputStream();
     }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+        int length = 0;
+        try {
+			s.setTcpNoDelay(true);
+            byte[] stream = new byte[3000];
+            
+            
+            length = this.in.read(stream);
+
+            
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        
+
         
     }
 }
